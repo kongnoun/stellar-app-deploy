e7dcd822114d92b21b2eed8d294e0a4496a4bcf2
kongnoun <kongnoun00@gmail.com>
2024-10-15 18:55:13 +0700

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Navbar from './assets/components/navbar'
-import LandingPage from './assets/components/Landing-page'
+import Navbar from './assets/components/Navbar.jsx'
+import LandingPage from './assets/components/Landing-page.jsx'
 import './index.css'
 
 createRoot(document.getElementById('Navbar')).render(
