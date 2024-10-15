e27322e42b63e2579118c682dcf53c8ba14d384b
kongnoun <kongnoun00@gmail.com>
2024-10-15 19:03:10 +0700

    5. trying to fix the image not displaying issue

--- src/assets/components/Landing-page.jsx
+++ src/assets/components/Landing-page.jsx
@@ -12,21 +12,21 @@ const LandingPage = () => {
         </div>
         <div className="carousel-inner rounded-3">
           <div className="carousel-item active">
-            <img src="src/assets/img/carousel1.jpg" className="d-block w-100" alt="..." />
+            <img src="./assets/img/carousel1.jpg" className="d-block w-100" alt="..." />
             <div className="carousel-caption d-none d-md-block">
               <h5>First slide label</h5>
               <p>Some representative placeholder content for the first slide.</p>
             </div>
           </div>
           <div className="carousel-item">
-            <img src="src/assets/img/carousel1.jpg" className="d-block w-100" alt="..." />
+            <img src="./assets/img/carousel1.jpg" className="d-block w-100" alt="..." />
             <div className="carousel-caption d-none d-md-block">
               <h5>Second slide label</h5>
               <p>Some representative placeholder content for the second slide.</p>
             </div>
           </div>
           <div className="carousel-item">
-            <img src="src/assets/img/carousel1.jpg" className="d-block w-100" alt="..." />
+            <img src="./assets/img/carousel1.jpg" className="d-block w-100" alt="..." />
             <div className="carousel-caption d-none d-md-block">
               <h5>Third slide label</h5>
               <p>Some representative placeholder content for the third slide.</p>
